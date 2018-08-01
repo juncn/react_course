39fb33efe07c97aca8d7f3179792576e532c996a
JunmingGuo <sjunmg@gmail.com>
2018-07-31 23:10:04 -0500

    Complete task 1

--- assignment-two/src/App.js
+++ assignment-two/src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputLength: 0
+  }
+
+  lengthChangeHandler = ({target}) => {
+    this.setState({inputLength: target.value.length});
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <input type="text" onChange={this.lengthChangeHandler} />
+        <p>{this.state.inputLength}</p>
       </div>
     );
   }
