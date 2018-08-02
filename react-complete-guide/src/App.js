73ab9b91d21a27c85cb740c9c8392c00c7bf700b
Guo, Junming <junming.guo@accenture.com>
2018-08-02 14:13:22 -0500

    Add ErrorBoundary component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './components/Person/Person';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -45,12 +46,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              key={person.id}
-              name={person.name}
-              age={person.age}
-              click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />;
+            return (
+              <ErrorBoundary key={person.id}>
+                <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.nameChangeHandler(event, person.id)} />
+              </ErrorBoundary>
+            )
           })}
         </div>
       );
