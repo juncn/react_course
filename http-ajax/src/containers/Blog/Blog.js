0e24bca237d17332e75b6694e795250012b38aed
Guo, Junming <junming.guo@accenture.com>
2018-10-15 17:03:43 -0500

    use link to switch pages

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 
@@ -17,12 +18,17 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New post</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New post</Link></li>
             </ul>
           </nav>
         </header>
         <Route exact path="/" component={Posts} />
+        <Route exact path="/new-post" component={NewPost} />
         {/* <section>
           <FullPost id={this.state.selectedPostId} />
         </section>
