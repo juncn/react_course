11a94db7705e402cf1bee83b3d0dff69cdddd3cd
juncn <sjunmg@gmail.com>
2018-10-16 12:00:57 -0500

    navigating programmatically

--- http-ajax/src/containers/Blog/Posts/Posts.js
+++ http-ajax/src/containers/Blog/Posts/Posts.js
@@ -29,7 +29,9 @@ class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
+    // this.setState({ selectedPostId: id });
+    this.props.history.push('/' + id);
+    // this.props.history.push({pathname: '/' + id});
   };
 
   render() {
@@ -37,13 +39,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          // <Link to={'/' + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
