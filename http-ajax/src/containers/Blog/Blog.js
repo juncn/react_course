75cf6dae97d3a32f7211272458c915dfca2e6791
juncn <sjunmg@gmail.com>
2018-10-17 14:53:30 -0500

    load routes lazily or asynchronouslly

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 import './Blog.css';
+// import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
+
 
 
 class Blog extends Component {
@@ -10,7 +15,7 @@ class Blog extends Component {
     posts: [],
     selectedPostId: null,
     error: false,
-    auth: false,
+    auth: true,
   };
 
   render() {
@@ -45,17 +50,10 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
           <Route path="/posts" component={Posts} />
           <Redirect from="/" to="/posts" />
         </Switch>
-        {/* TODO: remove */}
-        {/* <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section> */}
       </div>
     );
   }
