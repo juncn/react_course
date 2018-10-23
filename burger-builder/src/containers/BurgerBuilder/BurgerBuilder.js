ef15c005ee7ca281987b9706903cab9fd1591516
juncn <sjunmg@gmail.com>
2018-10-22 20:08:52 -0500

    add route in burger builder

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import axios from '../../axios-orders'
+import axios from '../../axios-orders';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -94,38 +94,47 @@ class BurgerBuilder extends Component {
 
   purchaseContinuedHandler = () => {
     // alert('You continue');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'John Smith',
-        address: {
-          street: '123 john smith st',
-          zipCode: '12345',
-          country: 'us'
-        },
-        email: 'johnsmith@test.com'
-      },
-      deliveryMethod: 'fastest'
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'John Smith',
+    //     address: {
+    //       street: '123 john smith st',
+    //       zipCode: '12345',
+    //       country: 'us'
+    //     },
+    //     email: 'johnsmith@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ 
+    //       loading: false,
+    //       purchasing: false,
+    //     });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ 
+    //       loading: false,
+    //       purchasing: false,
+    //     });
+    //   });
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ 
-          loading: false,
-          purchasing: false,
-        });
-      })
-      .catch(error => {
-        this.setState({ 
-          loading: false,
-          purchasing: false,
-        });
-      });
-    
+    const queryString = queryParams.join('&');
+
     // Navigate to '/checkout' page
-    this.props.history.push('/checkout');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   };
 
   render() {
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -5,11 +5,20 @@ class Checkout extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
+      salad: 0,
+      meat: 0,
+      cheese: 0,
+      bacon: 0
     }
+  }
+
+  componentDidMount() {
+    const tempIngredients = {};
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      tempIngredients[param[0]] = param[1];
+    }
+    this.setState({ingredients: tempIngredients});
   }
 
   checkoutCancelHandler = () => {
