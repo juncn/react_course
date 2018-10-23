e36541283a6c35bc15b622c8a2f0ef7522a86568
juncn <sjunmg@gmail.com>
2018-10-23 12:09:18 -0500

    add validation feedback

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -19,6 +19,7 @@ class ContactData extends Component {
           required: true
         },
         valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -31,6 +32,7 @@ class ContactData extends Component {
           required: true
         },
         valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -45,6 +47,7 @@ class ContactData extends Component {
           maxLength: 5
         },
         valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -57,6 +60,7 @@ class ContactData extends Component {
           required: true
         },
         valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -69,6 +73,7 @@ class ContactData extends Component {
           required: true
         },
         valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -132,6 +137,7 @@ class ContactData extends Component {
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
@@ -155,6 +161,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
