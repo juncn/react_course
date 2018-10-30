ecacb82170f0ed07cc837eb2ee6ffe257691bd38
juncn <sjunmg@gmail.com>
2018-10-30 15:50:22 -0500

    add Redirect when ingredients is not really

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,30 +1,10 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  // state = {
-  //   ingredients: null,
-  //   totalPrice: 0
-  // };
-
-  // componentWillMount() {
-  //   const tempIngredients = {};
-  //   const query = new URLSearchParams(this.props.location.search);
-  //   let price = 0;
-  //   for (let param of query.entries()) {
-  //     param[0] === 'price'
-  //       ? (price = param[1])
-  //       : (tempIngredients[param[0]] = +param[1]);
-  //   }
-  //   this.setState({
-  //     ingredients: tempIngredients,
-  //     totalPrice: price
-  //   });
-  // }
-
   checkoutCancelHandler = () => {
     this.props.history.goBack();
   };
@@ -34,19 +14,23 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ingredients}
-          checkoutCancelled={this.checkoutCancelHandler}
-          checkoutContinued={this.checkoutContinueHandler}
-        />
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ingredients) {
+      summary = (
+        <Fragment>
+          <CheckoutSummary
+            ingredients={this.props.ingredients}
+            checkoutCancelled={this.checkoutCancelHandler}
+            checkoutContinued={this.checkoutContinueHandler}
+          />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData}
+          />
+        </Fragment>
+      );
+    }
+    return {summary};
   }
 }
 
