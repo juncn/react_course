de1c1fa94a638e6511064575e7c13da3b42ff6f8
juncn <sjunmg@gmail.com>
2018-11-06 14:28:09 -0600

    get token from firebase backend

--- burger-builder/src/store/actions/authAction.js
+++ burger-builder/src/store/actions/authAction.js
@@ -1,4 +1,5 @@
 import actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -8,7 +9,7 @@ export const authStart = () => {
 
 export const authSuccess = authData => {
   return {
-    type: actionTypes.authSuccess,
+    type: actionTypes.AUTH_SUCCESS,
     payload: authData
   };
 };
@@ -23,5 +24,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBKst8Sjb_AHbcpzwWDpASmgPuKGTY2t9A',
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
